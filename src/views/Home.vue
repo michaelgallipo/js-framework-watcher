7f0925492a8fca6b0e90a2af9f4bdaf972439e69
jeannawong <jeannawong25@gmail.com>
2021-04-19 20:38:26 -0500

    Successful git requests to js repos for watcher data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="home">
     <h1>Javascript Framework Watcher</h1>
-    <img
-      alt="Javascript Logo"
-      src="../assets/javascript_logo_icon.png"
-      style="height: 128px"
-    />
+    <img alt="Javascript Logo" src="../assets/javascript_logo_icon.png" style="height: 128px" />
     <zingchart :data="myData"></zingchart>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import axios from "axios";
 
 export default {
   name: "home",
@@ -20,6 +17,11 @@ export default {
   // }
   data() {
     return {
+      vueData: {},
+      angularData: {},
+      emberData: {},
+      svelteData: {},
+      reactData: {},
       myData: {
         layout: "horizontal",
         graphset: [
@@ -68,6 +70,43 @@ export default {
         ],
       },
     };
+  },
+  created: function() {
+    axios.get("https://api.github.com/repos/vuejs/vue").then(response => {
+      console.log("vuejs repo", response);
+      this.vueData.subscribers = response.data.subscribers_count;
+      this.vueData.stars = response.data.stargazers_count;
+      this.vueData.forks = response.data.forks_count;
+      console.log(this.vueData);
+    });
+    axios.get("https://api.github.com/repos/angular/angular.js").then(response => {
+      console.log("angularjs repo", response);
+      this.angularData.subscribers = response.data.subscribers_count;
+      this.angularData.stars = response.data.stargazers_count;
+      this.angularData.forks = response.data.forks_count;
+      console.log(this.angularData);
+    });
+    axios.get("https://api.github.com/repos/emberjs/ember.js").then(response => {
+      console.log("emberjs repo", response);
+      this.emberData.subscribers = response.data.subscribers_count;
+      this.emberData.stars = response.data.stargazers_count;
+      this.emberData.forks = response.data.forks_count;
+      console.log(this.emberData);
+    });
+    axios.get("https://api.github.com/repos/sveltejs/svelte").then(response => {
+      console.log("svelte repo", response);
+      this.svelteData.subscribers = response.data.subscribers_count;
+      this.svelteData.stars = response.data.stargazers_count;
+      this.svelteData.forks = response.data.forks_count;
+      console.log(this.svelteData);
+    });
+    axios.get("https://api.github.com/repos/facebook/react").then(response => {
+      console.log("react repo", response);
+      this.reactData.subscribers = response.data.subscribers_count;
+      this.reactData.stars = response.data.stargazers_count;
+      this.reactData.forks = response.data.forks_count;
+      console.log(this.reactData);
+    });
   },
 };
 </script>
